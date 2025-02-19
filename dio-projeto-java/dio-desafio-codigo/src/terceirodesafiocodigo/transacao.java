682509f439e0dbd23c96b3d82a23652f17a864d4
SavioMariano <saviomariano77@gmail.com>
2025-02-19 14:38:27 -0300

    ADD: desafio do bootcamp dio

---
+++ dio-projeto-java/dio-desafio-codigo/src/terceirodesafiocodigo/transacao.java
@@ -0,0 +1,27 @@
+package terceirodesafiocodigo;
+
+import java.util.Scanner;
+
+public class transacao {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Entrada do saldo inicial
+        double saldoInicial = scanner.nextDouble();
+
+        // TODO: Na linha abaixo, implemente a entrada das três transações:
+        double transacaoUm = scanner.nextDouble();
+        double transacaoDois = scanner.nextDouble();
+        double transacaoTres = scanner.nextDouble();
+        // TODO: Na linha abaixo, realize o cálculo do saldo final:
+
+        if (transacaoUm > 0.0) {
+            
+        }
+        
+        // Saldo final
+        System.out.printf("%.2f\n", saldoFinal);
+
+        scanner.close();
+    }
+}
